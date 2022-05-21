53842bb74096a519308e0af97a5cc3ecb3323463
Näckel, Danny <danny.naeckel@student.hs-anhalt.de>
2022-05-21 13:41:21 +0200

    Update PetShelterTest.java

--- Uebung8/src/one/PetShelterTest.java
+++ Uebung8/src/one/PetShelterTest.java
@@ -1,5 +1,6 @@
 package one;
 
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -38,31 +39,112 @@ public class PetShelterTest
     static Animal animalSunfish = new Sunfish("tiersonne");
 
     @Test // 1
-    void testSay()
+    void testAnimal()
     {
-        assertEquals("meow",catCat.say());
-        assertEquals("woof",dogDog.say());
+        assertEquals("silence",animal.say());
+        assertEquals("walk",animal.move());
 
-        //mammal hat keine overwriting say(), doch cat ist datentyp cat und überschreibt say() [analog dasselbe wie dog]
-        assertEquals("meow",catMammal.say());
-        assertEquals("woof",dogMammal.say());
-
-        //super(mammal) hat kein overwriting say() -> animal -> animal.say() = "silence"
-        assertEquals("silence",mammal.say());
         assertEquals("silence",animalMammal.say());
+        assertEquals("walk",animalMammal.move());
+
+        assertEquals("meow",animalCat.say());
+        assertEquals("walk",animalCat.move());
+
+        assertEquals("woof",animalDog.say());
+        assertEquals("walk",animalDog.move());
+
+        assertEquals("tweet",animalBird.say());
+        assertEquals("fly",animalBird.move());
+
+        assertEquals("ssssssssss",animalLizard.say());
+        assertEquals("crawl",animalLizard.move()); // (super)lizard -> crawl
+
+        assertEquals("silence",animalReptile.say());
+        assertEquals("crawl",animalReptile.move());
+
+        assertEquals("blub",animalFish.say());
+        assertEquals("swim",animalFish.move());
+
+        assertEquals("blub",animalSunfish.say());
+        assertEquals("swim",animalSunfish.move());
+
+        assertEquals("noot noot",animalPenguin.say());
+        assertEquals("waddle",animalPenguin.move());
     }
 
     @Test // 3
-    void testMove()
+    void testMammal()
     {
-        assertEquals("fly",bird.move());
-        assertEquals("fly",animalBird.move());
 
-        assertEquals("waddle",penguinBird.move());
-        assertEquals("waddle",penguin.move());
+        //mammal hat keine overwriting say(), doch cat ist datentyp cat und überschreibt say() [analog dasselbe wie dog]
+        assertEquals("woof",dogMammal.say());
+        assertEquals("walk",dogMammal.move());
+
+        assertEquals("meow",catMammal.say());
+        assertEquals("walk",catMammal.move());
+
+        //super(mammal) hat kein overwriting say() -> animal -> animal.say() = "silence"
+        assertEquals("silence",mammal.say());
+        assertEquals("walk",mammal.move());
     }
 
     @Test // 6
+    void testReptile()
+    {
+        assertEquals("silence",reptile.say());
+        assertEquals("crawl",reptile.move());
+
+        assertEquals("ssssssssss",lizardReptile.say());
+        assertEquals("crawl",lizardReptile.move());
+    }
+
+    void testFish(){
+
+        assertEquals("blub",fish.say());
+        assertEquals("swim",fish.move());
+
+        //sunfish hat keine überschreibenden Methoden
+        assertEquals("blub",fishSun.say());
+        assertEquals("swim",fishSun.move());
+    }
+
+    void testBird(){
+
+        assertEquals("tweet",bird.say());
+        assertEquals("fly",bird.move());
+
+        assertEquals("noot noot",penguinBird.say());
+        assertEquals("waddle",penguinBird.move());
+
+    }
+
+    void testDog(){
+        assertEquals("woof",dogDog.say());
+        assertEquals("walk",dogDog.move());
+    }
+
+    void testCat(){
+
+        assertEquals("meow",catCat.say());
+        assertEquals("walk",catCat.move());
+    }
+
+    void testLizard(){
+
+        assertEquals("ssssssssss",lizard.say());
+        assertEquals("crawl",lizard.move());
+    }
+
+    void testPenguin(){
+        assertEquals("noot noot",penguin.say());
+        assertEquals("waddle",penguin.move());
+    }
+    void testSunfish(){
+
+        assertEquals("blub",sunfish.say());
+        assertEquals("swim",sunfish.move());
+    }
+
     void testPolymorph()
     {
         assertInstanceOf(lizard.getClass(),animalLizard);
@@ -70,6 +152,4 @@ public class PetShelterTest
         assertInstanceOf(lizard.getClass().getSuperclass(),reptile);
         assertInstanceOf(lizard.getClass().getSuperclass().getSuperclass(),animal);
     }
-
-
 }
